58f3a22902cdf0ecd5713fad99c71771e646061b
Samuel <samuel.sbase@gmail.com>
2025-09-23 12:29:43 +0200

    ejercicio30 hecho

--- PSP/workspace/EjerciciosIniciales/src/tarea1/ejercicio30.java
+++ PSP/workspace/EjerciciosIniciales/src/tarea1/ejercicio30.java
@@ -9,8 +9,12 @@ import java.util.Scanner;
 
 public class ejercicio30 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        SayHello();
+    }
 
-
+    class App {
+        public static void SayHello() {
+            System.out.println("Hello World");
+        }
     }
 }
