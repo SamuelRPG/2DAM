960c1b8195f4e3e6328cac1f69afe7651df74bcc
Samuel <samuel.sbase@gmail.com>
2025-09-23 12:17:50 +0200

    añadido bin y idea a gitignore

---
+++ PSP/workspace/EjerciciosIniciales/src/tarea1/ejercicio30.java
@@ -0,0 +1,16 @@
+//ya que este ejercicio es casi igual al anterior, uso ChatGPT
+//para darme ideas de validacion para variables introducidas por teclado.
+
+package tarea1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ejercicio30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
