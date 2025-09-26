748a76a41f3da951e749d33078bd743b7f99b773
Samuel <samuel.sbase@gmail.com>
2025-09-26 19:25:24 +0200

    creacion workspace prueba PSP

---
+++ PSP/1erTRIMESTRE/workspace-pruebas/src/LanzadorDeProcesos.java
@@ -0,0 +1,18 @@
+public class LanzadorDeProcesos {
+    public void ejecutar (String ruta) {
+        ProcessBuilder pb;
+        try {
+            pb = new ProcessBuilder(ruta);
+            pb.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String[] args) {
+        String ruta = "C:\\Windows\\System32\\calc.exe";
+        LanzadorDeProcesos lp = new LanzadorDeProcesos();
+        lp.ejecutar(ruta);
+        System.out.println("Finalizado");
+    }
+}
