156d4010bc205da9121e9f1bbe1d45337cded978
Samuel <samuel.sbase@gmail.com>
2025-10-06 14:26:00 +0200

    trabajo psp en casa

--- PSP/1erTRIMESTRE/workspace-pruebas/src/miniproyecto/CalculadoraNEO.java
+++ PSP/1erTRIMESTRE/workspace-pruebas/src/miniproyecto/CalculadoraNEO.java
@@ -1,4 +1,45 @@
 package miniproyecto;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculadoraNEO {
+    private static double CalcularProbabilidad(double pos, double vel) {
+        double posTierra=0;
+        for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
+            pos = pos + vel * i;
+            posTierra= posTierra + vel * i;
+        }
+        double resultado = 100 * Math.random() *
+                Math.pow( ((pos-posTierra/(pos+posTierra))), 2);
+        return resultado;
+    }
+
+    public static void RellenarFichero(File fichero, double pos, double vel, String NEO) {
+        try {
+            double probabilidad = CalcularProbabilidad(pos, vel);
+            fichero.createNewFile();
+            FileWriter fw = new FileWriter(fichero, StandardCharsets.UTF_8, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            if (probabilidad < 10) {
+                bw.write(NEO + ": " + String.format("%.2f", probabilidad) + "%");
+                bw.newLine();
+            } else {
+                System.err.println("NEO con probabilidad alta de impacto" + NEO + ": " + String.format("%.2f", probabilidad) + "%");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main (String[] args) {
+        String nomNEO = args[0];
+        double posNEO = Double.parseDouble(args[1]);
+        double velNEO = Double.parseDouble(args[2]);
+        File ficheroSalida = new File(args[3]);
+        RellenarFichero(ficheroSalida, posNEO, velNEO, nomNEO);
+    }
 }
--- PSP/1erTRIMESTRE/workspace-pruebas/src/miniproyecto/Lanzador.java
+++ PSP/1erTRIMESTRE/workspace-pruebas/src/miniproyecto/Lanzador.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lanzador {
-    public static void lanzador(int n1, int n2, String f) {
+    public static void lanzador(String f, String fSalida) {
         try {
             File ficheroNEO=new File(f);
             FileReader fr = new FileReader(f, StandardCharsets.UTF_8);
@@ -30,21 +30,39 @@ public class Lanzador {
                 command.add(String.valueOf(lineaArray[0]));
                 command.add(String.valueOf(lineaArray[1]));
                 command.add(String.valueOf(lineaArray[2]));
+                command.add(fSalida);
 
                 ProcessBuilder builder = new ProcessBuilder(command);
-
+                builder.inheritIO();
+                Process process = builder.start();
+                process.waitFor();
                 linea = br.readLine();
-
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        LeerFichero(fSalida);
+    }
 
+    public static void LeerFichero(String fLeido) {
+        try {
+            File ficheroLeido = new File (fLeido);
+            FileReader fr = new FileReader(ficheroLeido, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(fr);
+            String linea = br.readLine();
 
-
+             while (linea != null) {
+                System.out.println(linea);
+                linea=br.readLine();
+             }
+             br.close();
+             fr.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main (String[] args) {
-        lanzador(1, 50, "C:\\Users\\Usuario\\Documents\\2DAM\\PSP\\1erTRIMESTRE\\workspace-pruebas\\src\\miniproyecto\\NEO.txt");
+        lanzador("src/miniproyecto/NEO.txt", "src/miniproyecto/nomdelNEO.txt");
     }
 }
