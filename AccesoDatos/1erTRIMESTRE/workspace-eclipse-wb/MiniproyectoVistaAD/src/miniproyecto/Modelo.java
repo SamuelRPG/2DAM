84b7463ba2eb4e184c64e361e22fa111498cd1cd
Samuel <samuel.sbase@gmail.com>
2025-10-09 18:40:24 +0200

    cambios

--- AccesoDatos/1erTRIMESTRE/workspace-eclipse-wb/MiniproyectoVistaAD/src/miniproyecto/Modelo.java
+++ AccesoDatos/1erTRIMESTRE/workspace-eclipse-wb/MiniproyectoVistaAD/src/miniproyecto/Modelo.java
@@ -1,5 +1,29 @@
 package miniproyecto;
 
-public class Modelo {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class Modelo {
+	public int BuscarCoincidencias(String busqueda, String texto) {
+		int contador = 0;
+		String palabraBuscada = busqueda.toLowerCase();
+		String linea;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(texto));
+			while ((linea = br.readLine()) != null) {
+				String[] palabras = linea.toLowerCase().split("//W+");
+				for (String elem : palabras) {
+					if (elem == palabraBuscada) contador++;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contador;
+	}
+	
+	public int Reemplazar(String reemplazo, String texto) {
+		
+	}
 }
--- AccesoDatos/1erTRIMESTRE/workspace-eclipse-wb/MiniproyectoVistaAD/src/miniproyecto/Vista.java
+++ AccesoDatos/1erTRIMESTRE/workspace-eclipse-wb/MiniproyectoVistaAD/src/miniproyecto/Vista.java
@@ -3,10 +3,17 @@ package miniproyecto;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class Vista {
 
 	private JFrame frame;
+	private JTextField textFieldBuscar;
+	private JTextField textFieldReemplazar;
+	private JButton btnBuscar;
+	private JButton btnReemplazar;
 
 	/**
 	 * Launch the application.
@@ -36,8 +43,47 @@ public class Vista {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 733, 460);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textFieldBuscar = new JTextField();
+		textFieldBuscar.setBounds(23, 185, 197, 20);
+		frame.getContentPane().add(textFieldBuscar);
+		textFieldBuscar.setColumns(10);
+		
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(232, 184, 89, 23);
+		frame.getContentPane().add(btnBuscar);
+		
+		textFieldReemplazar = new JTextField();
+		textFieldReemplazar.setBounds(386, 185, 172, 20);
+		frame.getContentPane().add(textFieldReemplazar);
+		textFieldReemplazar.setColumns(10);
+		
+		btnReemplazar = new JButton("Reemplazar");
+		btnReemplazar.setBounds(568, 184, 89, 23);
+		frame.getContentPane().add(btnReemplazar);
+		
+		JTextArea textAreaInput = new JTextArea();
+		textAreaInput.setBounds(23, 11, 684, 163);
+		frame.getContentPane().add(textAreaInput);
+		
+		JTextArea textAreaChanges = new JTextArea();
+		textAreaChanges.setBounds(23, 230, 684, 169);
+		frame.getContentPane().add(textAreaChanges);
 	}
-
+	
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+	public JButton getBtnReemplazar() {
+		return btnReemplazar;
+	}
+	public JTextField getTextFieldBuscar() {
+		return textFieldBuscar;
+	}
+	public JTextField getTextFieldReemplazar() {
+		return textFieldReemplazar;
+	}
 }
